65f94e9d6d6b13814db7b1173383b17f8429a100
Admin <Admin@MT0314>
2026-01-23 09:25:03 +0700

    up

---
+++ Forms/FrmLoaiSanPham.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace QuanLyBanHang.Forms
+{
+    partial class FrmLoaiSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            txtTenLoai = new TextBox();
+            btnThem = new Button();
+            btnSua = new Button();
+            btnXoa = new Button();
+            btnLuu = new Button();
+            btnHuyBo = new Button();
+            btnThoat = new Button();
+            groupBox2 = new GroupBox();
+            dataGridView = new DataGridView();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnThoat);
+            groupBox1.Controls.Add(btnHuyBo);
+            groupBox1.Controls.Add(btnLuu);
+            groupBox1.Controls.Add(btnXoa);
+            groupBox1.Controls.Add(btnSua);
+            groupBox1.Controls.Add(btnThem);
+            groupBox1.Controls.Add(txtTenLoai);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(723, 139);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin loại sản phẩm";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Tên loại sản phẩm (*):";
+            // 
+            // txtTenLoai
+            // 
+            txtTenLoai.Location = new Point(127, 42);
+            txtTenLoai.Name = "txtTenLoai";
+            txtTenLoai.Size = new Size(576, 23);
+            txtTenLoai.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(127, 92);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(75, 23);
+            btnThem.TabIndex = 2;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(221, 92);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(75, 23);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.ForeColor = Color.Red;
+            btnXoa.Location = new Point(323, 92);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(75, 23);
+            btnXoa.TabIndex = 4;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            btnLuu.ForeColor = SystemColors.Highlight;
+            btnLuu.Location = new Point(433, 92);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(75, 23);
+            btnLuu.TabIndex = 5;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnHuyBo
+            // 
+            btnHuyBo.Location = new Point(534, 92);
+            btnHuyBo.Name = "btnHuyBo";
+            btnHuyBo.Size = new Size(75, 23);
+            btnHuyBo.TabIndex = 6;
+            btnHuyBo.Text = "Hủy bỏ";
+            btnHuyBo.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(628, 92);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(75, 23);
+            btnThoat.TabIndex = 7;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView);
+            groupBox2.Location = new Point(12, 157);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(723, 291);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(0, 22);
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new Size(723, 269);
+            dataGridView.TabIndex = 0;
+            // 
+            // FrmLoaiSanPham
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "FrmLoaiSanPham";
+            Text = "Loại sản phẩm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnThoat;
+        private Button btnHuyBo;
+        private Button btnLuu;
+        private Button btnXoa;
+        private Button btnSua;
+        private Button btnThem;
+        private TextBox txtTenLoai;
+        private Label label1;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView;
+    }
+}
